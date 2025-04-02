9ef20bfeb8b6fe8ce937801329cbeb6cf11f4a7b
ZiDXie <shengxie47@gmail.com>
2025-04-02 19:54:06 +0800

    Implement MyQueue using two stacks; add push, pop, peek, and empty methods.

--- c cpp/ 刷题/src/Stack_Queue/MyQueue.cpp
+++ c cpp/ 刷题/src/Stack_Queue/MyQueue.cpp
@@ -1,20 +1,45 @@
 //
 // Created by xie on 25-4-2.
 //
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
 class MyQueue {
 public:
+    stack<int> in;
+    stack<int> out;
+
     MyQueue() {
     }
 
     void push(int x) {
+        in.push(x);
     }
 
     int pop() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        int res = out.top();
+        out.pop();
+        return res;
     }
 
     int peek() {
+        int res = this->pop();
+        out.push(res);
+        return res;
     }
 
     bool empty() {
+        if (in.empty() && out.empty()) {
+            return true;
+        }
+        return false;
     }
 };
